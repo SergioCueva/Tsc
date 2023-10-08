d021ab56a31b6951e13ad6f538371fc9c2fedf0f
scueva <carlos_2580_4@hotmail.com>
2023-10-08 11:25:18 -0500

    variables enum type script

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,29 @@ mensaje = "chanchito feliz";
 mensaje = "chao mundo";
 console.log(mensaje);
 console.log(typeof []);
+let extincionDinosarios = 7600000;
+let dinosariosFavoritos = "T-Rex";
+let extintos = true;
+function chanchitofeliz(config) {
+    return config;
+}
+let animales = ['chanchito', 'feliz', 'felipe'];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let tupla = [1, ['chanchito feliz', "chanchito felipe"]];
+const chica = 's';
+const mediana = 'm';
+const grande = 'l';
+const extragrande = 'xl';
+var Talla;
+//ifee ->inmediate invoke function expresion
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,64 @@ mensaje="chao mundo"
 
 console.log(mensaje)
 console.log(typeof [])
-
-/**
+/*
+ * tipos de JS
+ * 
  * number
- * suntring
+ * string
  * boolean
  * null
  * undefined
  * object
  * function
+ * 
+ * tipos de TS
+ * any ---no usarlo 
+ * unknown
+ * never
+ * array
+ * tuplas
+ * Enums
+ * 
+ * tipos inferidos
+ */ 
+/*puedes colocar el equipadode tipo de datos o no es necesario*/
+let extincionDinosarios=76_000_00
+let dinosariosFavoritos="T-Rex"
+let extintos=true
+
+function chanchitofeliz(config:any){
+    return config
+}
+
+/*arreglo*/
+
+let animales:string[]=['chanchito','feliz','felipe']
+let nums: number[]=[1,2,3]
+let checks:boolean[]=[]
+let nums2:Array<Number>=[]
+
+//nums.map(x=> x.)// el autocompletado sugiere metodos de tipo de dato
+
+/*tuplas una variable que contiene un set de datos ordenados */
+
+let tupla:[number,String[]]=[1,['chanchito feliz',"chanchito felipe"]]
+
+const chica='s'
+const mediana='m'
+const grande='l'
+const extragrande='xl'
+//pascalcase
+enum Talla{Chica='s',Mediana='m',Grande='l',ExtraGrande='xl'}
+
+const variable1 = Talla.Grande
+
+console.log(variable1)
+
+
+
+
+
  
- */
+
+ 
